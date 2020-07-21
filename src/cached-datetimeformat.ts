51433575f6a9a728a6a1edc84b245348823580b9
Juraj Kapsiar <jukapsia@microsoft.com>
2020-07-21 19:15:34 +0200

    avoid es version specific Intl interface

--- src/cached-datetimeformat.ts
+++ src/cached-datetimeformat.ts
@@ -9,13 +9,13 @@ export class CachedDateTimeFormat {
   // We're keying this using JSON.stringify because with a WeakMap we've have a key pair
   // (locale - string & options - object) and stringify is native so it is so fars it is
   // not worth maintaing the two-level cache (map for string and weak map for object)
-  private readonly localeFormatCache = new Map<string, SafeDateTimeFormat>();
+  private readonly localeFormatCache = new Map<string, Intl.DateTimeFormat>();
 
   public get(locale: string, dateTimeOptions: Intl.DateTimeFormatOptions) {
     const key = `${locale}:${JSON.stringify(dateTimeOptions)}`;
     let dtf = this.localeFormatCache.get(key);
     if (!dtf) {
-      dtf = new SafeDateTimeFormat(locale, dateTimeOptions);
+      dtf = SafeDateTimeFormat(locale, dateTimeOptions);
       this.localeFormatCache.set(key, dtf);
     }
 
--- src/safe-datetimeformat.ts
+++ src/safe-datetimeformat.ts
@@ -3,54 +3,41 @@
  * Licensed under the MIT License.
  */
 
+let _fallbackNeeded: boolean | undefined = undefined;
+const fallbackNeeded = (locales?: string | string[] | undefined) => {
+  if (_fallbackNeeded === undefined) {
+    const format = Intl.DateTimeFormat(locales);
+    const tz = format.resolvedOptions().timeZone;
+    if (!tz || tz.toLowerCase() === 'etc/unknown') {
+      _fallbackNeeded = true;
+    } else {
+      _fallbackNeeded = false;
+    }
+  }
+
+  return _fallbackNeeded;
+};
+
+const getOptionsWithFallback = (locales?: string | string[] | undefined, options?: Intl.DateTimeFormatOptions) => {
+  if (options && options['timeZone']) {
+    return options;
+  }
+
+  if (!fallbackNeeded(locales)) {
+    return options;
+  }
+
+  return {
+    ...options,
+    timeZone: 'UTC'
+  };
+};
+
 /**
  * Intl.DateTimeFormat can throw an exception when it does not detect time zone
  * If time zone is not specified, use fallback to UTC instead of throwing an exception
  */
-export class SafeDateTimeFormat {
-  private static _fallbackNeeded: boolean | undefined = undefined;
-  private static fallbackNeeded = (locales?: string | string[] | undefined) => {
-    if (SafeDateTimeFormat._fallbackNeeded === undefined) {
-      const format = Intl.DateTimeFormat(locales);
-      const tz = format.resolvedOptions().timeZone;
-      if (!tz || tz.toLowerCase() === 'etc/unknown') {
-        SafeDateTimeFormat._fallbackNeeded = true;
-      } else {
-        SafeDateTimeFormat._fallbackNeeded = false;
-      }
-    }
-
-    return SafeDateTimeFormat._fallbackNeeded;
-  }
-
-  private _format: Intl.DateTimeFormat;
-
-  private getOptionsWithFallback(locales?: string | string[] | undefined, options?: Intl.DateTimeFormatOptions) {
-    if (options && options['timeZone']) {
-      return options;
-    }
-
-    const fallbackNeeded = SafeDateTimeFormat.fallbackNeeded(locales);
-    if (!fallbackNeeded) {
-      return options;
-    }
-
-    return {
-      ...options,
-      timeZone: 'UTC'
-    };
-  }
-
-  constructor(locales?: string | string[] | undefined, options?: Intl.DateTimeFormatOptions) {
-    const optionsWithFallback = this.getOptionsWithFallback(locales, options);
-    this._format = Intl.DateTimeFormat(locales, optionsWithFallback);
-  }
-
-  public format(date?: number | Date | undefined): string {
-    return this._format.format(date);
-  }
-
-  public formatToParts(date?: number | Date | undefined): Intl.DateTimeFormatPart[] {
-    return this._format.formatToParts(date);
-  }
-}
+export const SafeDateTimeFormat = (locales?: string | string[] | undefined, options?: Intl.DateTimeFormatOptions) => {
+  const optionsWithFallback = getOptionsWithFallback(locales, options);
+  return Intl.DateTimeFormat(locales, optionsWithFallback);
+};
